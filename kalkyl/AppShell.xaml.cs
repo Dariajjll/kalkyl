b80d0530abbfd37e98134a6f5d4439d88626ed03
Danila <68828127+MINeSSTEIN@users.noreply.github.com>
2023-03-11 13:08:03 +0300

    Добавьте файлы проекта.

---
+++ kalkyl/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace kalkyl;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ kalkyl/MainPage.xaml.cs
@@ -0,0 +1,134 @@
+﻿namespace kalkyl;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonClear_Clicked(object sender, EventArgs e)
+    {
+        txtkalkyl.Text = "0";
+    }
+    private void ButtonClearOne_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str.Length <= 1) txtkalkyl.Text = "0";
+        else txtkalkyl.Text = str.Remove(str.Length - 1);
+    }
+    private void ButtonZero_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "0";
+        else txtkalkyl.Text = string.Concat(str, 0);
+    }
+    private void ButtonOne_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "1";
+        else txtkalkyl.Text = string.Concat(str, 1);
+    }
+    private void ButtonTwo_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "2";
+        else txtkalkyl.Text = string.Concat(str, 2);
+    }
+    private void ButtonThree_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "3";
+        else txtkalkyl.Text = string.Concat(str, 3);
+    }
+    private void ButtonFour_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "4";
+        else txtkalkyl.Text = string.Concat(str, 4);
+    }
+    private void ButtonFive_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "5";
+        else txtkalkyl.Text = string.Concat(str, 5);
+    }
+    private void ButtonSix_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "6";
+        else txtkalkyl.Text = string.Concat(str, 6);
+    }
+    private void ButtonSeven_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "7";
+        else txtkalkyl.Text = string.Concat(str, 7);
+    }
+    private void ButtonEight_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "8";
+        else txtkalkyl.Text = string.Concat(str, 8);
+    }
+    private void ButtonNine_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "9";
+        else txtkalkyl.Text = string.Concat(str, 9);
+    }
+    private void ButtonPlus_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "0";
+        else if (str.Contains("+") == false && str.Contains("-") == false && str.Contains("*") == false && str.Contains("/") == false) txtkalkyl.Text = string.Concat(str, "+");
+    }
+    private void ButtonMinus_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "0";
+        else if (str.Contains("+") == false && str.Contains("-") == false && str.Contains("*") == false && str.Contains("/") == false) txtkalkyl.Text = string.Concat(str, "-");
+    }
+    private void ButtonUmn_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "0";
+        else if (str.Contains("+") == false && str.Contains("-") == false && str.Contains("*") == false && str.Contains("/") == false) txtkalkyl.Text = string.Concat(str, "*");
+    }
+    private void ButtonDelen_Clicked(object sender, EventArgs e)
+    {
+        string str = txtkalkyl.Text;
+        if (str == "0") txtkalkyl.Text = "0";
+        else if (str.Contains("+") == false && str.Contains("-") == false && str.Contains("*") == false && str.Contains("/") == false) txtkalkyl.Text = string.Concat(str, "/");
+    }
+    private void ButtonRavno_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string s = txtkalkyl.Text;
+
+            char[] operat = { '+', '-', '*', '/' };
+
+            var data = s.Split(operat, StringSplitOptions.None);
+            int arg1 = int.Parse(data[0]);
+            int arg2 = int.Parse(data[1]);
+
+            if (s.Contains("-")) txtkalkyl.Text = (arg1 - arg2).ToString();
+            else if (s.Contains("+")) txtkalkyl.Text = (arg1 + arg2).ToString();
+            else if (s.Contains("*")) txtkalkyl.Text = (arg1 * arg2).ToString();
+            else txtkalkyl.Text = (arg1 / arg2).ToString();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ошибка!", ex.ToString(), "OK");
+            txtkalkyl.Text = "0";
+        }
+    }
+    private void ButtonStep_Clicked(object sender, EventArgs e)
+    {
+        int str = int.Parse(txtkalkyl.Text);
+        if (str == 0) txtkalkyl.Text = "0";
+        else txtkalkyl.Text = Math.Pow(2, str).ToString();
+    }
+}
+
